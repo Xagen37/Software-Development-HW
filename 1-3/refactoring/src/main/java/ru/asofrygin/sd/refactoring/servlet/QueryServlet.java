cc36464dabbcccb34731d4c044887bcb0e3f197f
Xagen37 <xagen.rus@gmail.com>
2023-03-24 20:03:01 +0300

    1-3: Rewrite QueryServlet

--- 1-3/refactoring/src/main/java/ru/asofrygin/sd/refactoring/servlet/QueryServlet.java
+++ 1-3/refactoring/src/main/java/ru/asofrygin/sd/refactoring/servlet/QueryServlet.java
@@ -8,101 +8,88 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author akirakozov
  */
 public class QueryServlet extends HttpServlet {
+
+    private enum KeyWord {
+        COUNT, MAX, MIN, SUM, VOID;
+
+        public static KeyWord get(String strKeyWord) {
+            switch (strKeyWord) {
+                case "count": return COUNT;
+                case "max": return MAX;
+                case "min": return MIN;
+                case "sum": return SUM;
+                default: return VOID;
+            }
+        }
+
+        public static final Map<String, KeyWord> getKeyWord = new HashMap<>(Map.of(
+                "count", COUNT,
+                "max", MAX,
+                "min", MIN,
+                "sum", SUM
+        ));
+
+        public static final EnumMap<KeyWord, String> getQuery = new EnumMap<>(Map.of(
+                COUNT, "SELECT COUNT(*) FROM PRODUCT",
+                MAX, "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1",
+                MIN, "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1",
+                SUM, "SELECT SUM(price) FROM PRODUCT"
+        ));
+
+        public static final EnumMap<KeyWord, String> getHeader = new EnumMap<>(Map.of(
+                COUNT, "Number of products: ",
+                MAX, "<h1>Product with max price: </h1>",
+                MIN, "<h1>Product with min price: </h1>",
+                SUM, "Summary price: "
+        ));
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String command = request.getParameter("command");
-
-        if ("max".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("<h1>Product with max price: </h1>");
-
-                    while (rs.next()) {
-                        String  name = rs.getString("name");
-                        int price  = rs.getInt("price");
-                        response.getWriter().println(name + "\t" + price + "</br>");
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("min".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("<h1>Product with min price: </h1>");
-
-                    while (rs.next()) {
-                        String  name = rs.getString("name");
-                        int price  = rs.getInt("price");
-                        response.getWriter().println(name + "\t" + price + "</br>");
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("sum".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT SUM(price) FROM PRODUCT");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("Summary price: ");
-
-                    if (rs.next()) {
-                        response.getWriter().println(rs.getInt(1));
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else if ("count".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM PRODUCT");
-                    response.getWriter().println("<html><body>");
-                    response.getWriter().println("Number of products: ");
-
-                    if (rs.next()) {
-                        response.getWriter().println(rs.getInt(1));
-                    }
-                    response.getWriter().println("</body></html>");
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else {
+        KeyWord key = KeyWord.get(command);
+        if (key == KeyWord.VOID) {
             response.getWriter().println("Unknown command: " + command);
+            return;
+        }
+
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(KeyWord.getQuery.get(key));
+
+            response.getWriter().println("<html><body>");
+            response.getWriter().println(KeyWord.getHeader.get(key));
+
+            switch (KeyWord.get(command)) {
+                case COUNT:
+                case SUM:
+                    if (rs.next()) {
+                        response.getWriter().println(rs.getInt(1));
+                    }
+                    break;
+                case MAX:
+                case MIN:
+                    while (rs.next()) {
+                        String  name = rs.getString("name");
+                        int price  = rs.getInt("price");
+                        response.getWriter().println(name + "\t" + price + "</br>");
+                    }
+                    break;
+            }
+
+            response.getWriter().println("</body></html>");
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         response.setContentType("text/html");
