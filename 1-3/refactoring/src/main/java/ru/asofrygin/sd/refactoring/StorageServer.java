db5aefc206153e8a62a03ba60c6eaed6e823d091
Xagen37 <xagen.rus@gmail.com>
2023-03-24 18:28:36 +0300

    1-3: Move server startup from Main

---
+++ 1-3/refactoring/src/main/java/ru/asofrygin/sd/refactoring/StorageServer.java
@@ -0,0 +1,52 @@
+package ru.asofrygin.sd.refactoring;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import ru.asofrygin.sd.refactoring.servlet.AddProductServlet;
+import ru.asofrygin.sd.refactoring.servlet.GetProductsServlet;
+import ru.asofrygin.sd.refactoring.servlet.QueryServlet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * @author asofrygin
+ */
+public class StorageServer {
+
+    private final String url;
+    private final int port;
+
+    public StorageServer(String url, int port) {
+        this.url = url;
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        try (Connection c = DriverManager.getConnection(url)) {
+            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
+                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " PRICE          INT     NOT NULL)";
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+        }
+
+        Server server = new Server(port);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new AddProductServlet()), "/add-product");
+        context.addServlet(new ServletHolder(new GetProductsServlet()),"/get-products");
+        context.addServlet(new ServletHolder(new QueryServlet()),"/query");
+
+        server.start();
+        server.join();
+    }
+}
