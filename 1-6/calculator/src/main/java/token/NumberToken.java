b7c397a97dd743a6639e71bf35ac6c8ff5b57269
Xagen37 <xagen.rus@gmail.com>
2023-03-25 10:25:25 +0300

    1-6: initial commit

---
+++ 1-6/calculator/src/main/java/token/NumberToken.java
@@ -0,0 +1,10 @@
+package token;
+
+import visitor.TokenVisitor;
+
+public class NumberToken implements Token {
+    @Override
+    public void accept(TokenVisitor visitor) {
+
+    }
+}
---
+++ 1-6/calculator/src/main/java/token/Operation.java
@@ -0,0 +1,10 @@
+package token;
+
+import visitor.TokenVisitor;
+
+public class Operation implements Token {
+    @Override
+    public void accept(TokenVisitor visitor) {
+
+    }
+}
